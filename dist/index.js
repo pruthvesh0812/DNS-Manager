b146166e376c987f82f39c5f234427f4b3ae017f
pruthvesh <code.pruthvesh@gmail.com>
2024-03-31 12:51:26 +0530

    pushing to ec2

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+
 Object.defineProperty(exports, "__esModule", { value: true });
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_1 = __importDefault(require("express"));
